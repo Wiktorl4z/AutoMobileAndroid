8a2443641bf17f726e99149d7dae3079d78de88a
Wiktor <wiktorkalinowski@gmail.com>
2017-10-11 18:41:55 +0200

    refactor

--- app/src/main/java/com/example/android/myapplication/TestActivity.java
+++ app/src/main/java/com/example/android/myapplication/TestActivity.java
@@ -31,42 +31,19 @@ public class TestActivity extends AppCompatActivity {
     private MyWebService service;
     static List<Car> items;
 
-    /*    @BindView(R.id.cars_list)
-        View cars_layout;*/
     @BindView(R.id.spinner_brand)
     Spinner spinnerBrand;
     @BindView(R.id.spinner_type)
     Spinner spinnerType;
     @BindView(R.id.spinner_year)
     Spinner spinnerYeah;
-/*    @BindView(R.id.button_checker)
-    Button buttonChecker;
-    @BindView(R.id.text_view_car_id)
-    TextView textViewCarId;
-    @BindView(R.id.text_view_car_name)
-    TextView textViewCarName;
-    @BindView(R.id.text_view_car_price)
-    TextView textViewCarPrice;
-    @BindView(R.id.text_view_car_engine)
-    TextView textViewCarEngine;
-    @BindView(R.id.text_view_car_passenger)
-    TextView textViewCarPassenger;*/
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cars_layout);
 
         ButterKnife.bind(this);
-        //   ButterKnife.bind(this, cars_layout);
 
-        View inflatedView = getLayoutInflater().inflate(R.layout.cars_list, null);
-
-        TextView textViewCarId = (TextView) inflatedView.findViewById(R.id.text_view_car_id);
-        TextView textViewCarName = (TextView) inflatedView.findViewById(R.id.text_view_car_name);
-        TextView textViewCarPrice = (TextView) inflatedView.findViewById(R.id.text_view_car_price);
-        TextView textViewCarEngine = (TextView) inflatedView.findViewById(R.id.text_view_car_engine);
-        TextView textViewCarPassenger = (TextView) inflatedView.findViewById(R.id.text_view_car_passenger);
-        ImageView imageView = (ImageView) inflatedView.findViewById(R.id.image_view_car);
         Button buttonChecker = (Button) findViewById(R.id.button_checker);
 
         service = HttpConnector.getService(MyWebService.class);
@@ -82,31 +59,9 @@ public class TestActivity extends AppCompatActivity {
                     public void onResponse(Call<List<Car>> call, Response<List<Car>> response) {
                         items = response.body();
 
-                        for (int i = 0; i < items.size(); i++) {
-                            Car car = items.get(i);
-                            int id = car.getId();
-
-                            imageView.setImageResource(R.drawable.jeep);
-
-                            textViewCarId.setText(id + "");
-
-                            String name = car.getName();
-                            textViewCarName.setText(name);
-
-                            String priceString = car.getPrice().toString();
-                            textViewCarPrice.setText(priceString);
-
-                            String engine = car.getEngine();
-                            textViewCarEngine.setText(engine);
-
-                            String passenger = car.getEngine();
-                            textViewCarPassenger.setText(passenger);
-                            Log.d(CLASS_TAG, car.getName());
-
-                            Adapter adapter = new Adapter(TestActivity.this, items);
-                            ListView listView = (ListView) findViewById(R.id.list_view);
-                            listView.setAdapter(adapter);
-                        }
+                        Adapter adapter = new Adapter(TestActivity.this, items);
+                        ListView listView = (ListView) findViewById(R.id.list_view);
+                        listView.setAdapter(adapter);
                     }
 
                     @Override
